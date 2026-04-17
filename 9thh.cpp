0a6b56631d287bdec84648689ff98a9ccdd32494
malaika-aqeel <malaikaaaaqeel@gmail.com>
2026-04-17 22:29:05 +0500

    Add files via upload

---
+++ 9thh.cpp
@@ -0,0 +1,17 @@
+//Question 9. Write a code to display ASSCI values of alphabets using explicit 
+//type casting. 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter character: ";
+    cin >> ch;
+
+    cout << "ASCII value = " << (int)ch;
+
+    return 0;
+}
